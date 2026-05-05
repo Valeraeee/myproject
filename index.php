8828cfec60a20dd8946f319fe53f0c75fe9b43f5
=Valerie <=S10270376@connect.np.edu.sg>
2026-05-05 13:29:48 +0800

    Initial commit: Add 2 files

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
+
 // Sample PHP file
-$name = "Mr CNAS";
-<<<<<<< HEAD
-<<<<<<< HEAD
-$message = "Hello, $name! Welcome to  CNAS 444.";
-=======
-$message = "Hello, $name! Welcome to  CNAS 778.";
->>>>>>> a275c8d88bfbbd9202f520ebbf6ea1259119edb6
-=======
+
+$name = "Ms Valerie Soh -CNAS";
+
 $message = "Hello, $name! Welcome to  CNAS 1.";
->>>>>>> 6e35a514eac475c946139db9710d1833d04270df
+
 echo $message;
+
 ?>
+
 
